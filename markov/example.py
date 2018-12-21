7b73e2aa5f2e9054fffd00524feba3d9b706e126
Edmaniak <adam.ouhrabka@gmail.com>
2018-12-21 10:36:44 +0100

    data

--- markov/example.py
+++ markov/example.py
@@ -1,11 +1,26 @@
 import matplotlib.image as img
+from markov.mark import Image as IMG, Model
+import imageio
+
+from PIL import Image
+import numpy as np
+import image_slicer
+import glob
+data = []
+files = glob.glob("troo/*.jpg")
+
+for a in range(0,10):
+    for i in range(0, 19):
+        image = imageio.imread(files[i])
+        data.append(IMG(image))
+        print(str(i) + " read")
 
 
 
 
-# Load image
+model = Model(data,0)
+model.compile()
+model.generate(20)
 
-# data = []
-# for i in range(1, 20):
-#    image = img.imread('./data/' + str(i) + '.jpg')
-#   data.append(Image(image, image.shape[0], image.shape[1]))
+a = 5
+# print("ahoj")
--- markov/test.py
+++ markov/test.py
@@ -13,12 +13,12 @@ obj6 = np.array([[[0, 0, 0], [255, 255, 255], [0, 0, 0]], [[255, 255, 255], [255
 
 z = np.zeros((2,3))
 
-img1 = Image(obj1, 3, 2)
-img2 = Image(obj2, 3, 2)
-img3 = Image(obj3, 3, 2)
-img4 = Image(obj4, 3, 2)
-img5 = Image(obj5, 3, 2)
-img6 = Image(obj6, 3, 2)
+img1 = Image(obj1)
+img2 = Image(obj2)
+img3 = Image(obj3)
+img4 = Image(obj4)
+img5 = Image(obj5)
+img6 = Image(obj6)
 
 data = []
 
@@ -31,6 +31,9 @@ data.append(img6)
 
 model = Model(data)
 model.compile()
+picture = model.generate(1)
+
+
 
 a = 5
 
