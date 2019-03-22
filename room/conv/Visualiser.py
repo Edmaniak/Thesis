34f755cf040bb3e9e915959dc85f16b519e1cd8f
Edmaniak <adam.ouhrabka@gmail.com>
2019-03-22 16:05:26 +0100

    Postup + testy

--- room/conv/Visualiser.py
+++ room/conv/Visualiser.py
@@ -3,11 +3,16 @@ import uuid
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.patches as mpatches
+import matplotlib as mpl
 
 
 class Visualiser:
     def visualise_array(self, array, save=False):
-        im = plt.imshow(array)
+        colors = ['white', 'black', 'orange', 'blue', 'green', 'purple', 'red', 'yellow', 'grey']
+        bounds = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        cmap = mpl.colors.ListedColormap(colors)
+        norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+        im = plt.imshow(array, interpolation='none', cmap='Paired', norm=norm)
         values = np.unique(array.ravel())
         # get the colors of the values, according to the
         # colormap used by imshow
@@ -15,7 +20,7 @@ class Visualiser:
         # create a patch (proxy artist) for every color
         patches = [mpatches.Patch(color=colors[i], label="Třída {l}".format(l=values[i])) for i in range(len(values))]
         # put those patched as legend-handles into the legend
-        plt.legend(handles=patches, loc='upper center', bbox_to_anchor=(0.5, -0.2),
+        plt.legend(handles=patches, loc='upper center', bbox_to_anchor=(0.5, -0.05),
                    fancybox=True, shadow=True, ncol=len(values))
 
         plt.show()
--- room/conv/dictionary.py
+++
@@ -1,8 +0,0 @@
-dictionary = {
-    0: "Empty",
-    1: "Wall",
-    2: "Dining table",
-    3: "Chair",
-    7: "Window",
-    10: "Door,",
-}
